b1aef4fdb3fb75aff36d8d177d314ca533f180d7
danielnahhas <danielnahhas1999@gmail.com>
2024-03-22 15:18:03 +0200

    Content for isp

--- src/Compoments/InternetIsp/InternetIsp.jsx
+++ src/Compoments/InternetIsp/InternetIsp.jsx
@@ -3,13 +3,63 @@ import './InternetIsp.css';
 
 function InternetIsp() {
   return (
-    <div>
-
-<div class="cardi">
-    <h2>Residential</h2>
-</div>
+    <div className='int-body'>
+      <div className="cardi">
+        <h2>BUNDLES</h2>
+        <ul>
+          <li>
+            <span className="left-content">6M - 8M</span>
+            <span className="price">$30</span><span>/MO</span>
+          </li>
+          <li>
+            <span className="left-content">7M - 10M</span>
+            <span className="price">$35</span><span>/MO</span>
+          </li>
+          <li>
+            <span className="left-content">8M - 12M</span>
+            <span className="price">$45</span><span>/MO</span>
+          </li>
+          <li>
+            <span className="left-content">9M - 16M</span>
+            <span className="price">$55</span><span>/MO</span>
+          </li>
+          <li>
+            <span className="left-content">15M - 30M</span>
+            <span className="price">$75</span><span>/MO</span>
+          </li>
+          <li>
+            <span className="left-content">30M - 50M</span>
+            <span className="price">$120</span><span>/MO</span>
+          </li>
+        </ul>
+      </div>
+      
+      <div className="additional-content">
+          <div className="upgrade-card">
+            <h3>Upgrades</h3>
+            <div className="upgrade-option">
+            <div className="upgrade-option">
+  <div className="cardi">
+    <h2>Gaming</h2>
+    <div className="price-container">
+      <span className="price">$15</span>
+      <span className="price-sign">/MO</span>
     </div>
-  )
+  </div>
+  <div className="cardi">
+    <h2>Real IP</h2>
+    <div className="price-container">
+      <span className="price">$25</span>
+      <span className="price-sign">/MO</span>
+    </div>
+  </div>
+</div>
+
+            </div>
+          </div>
+        </div>
+    </div>
+  );
 }
 
-export default InternetIsp
+export default InternetIsp;
