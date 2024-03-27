968c9c3be7edea718faec13f71e9f2142b4fd0ca
danielnahhas <danielnahhas1999@gmail.com>
2024-03-27 16:55:03 +0200

    padding

--- src/Compoments/Nav/Nav.jsx
+++ src/Compoments/Nav/Nav.jsx
@@ -47,6 +47,7 @@ function Nav() {
             <button><Link to={"/4g-internet"}>4G Internet</Link></button>
           </div>
         </div>
+        <Link className='nav' to={"/"}>Home</Link>
       </div>
       <div className='icon' onClick={toggleMenu}>
         <div className={`bar ${isOpen ? 'animate' : ''}`}></div>
